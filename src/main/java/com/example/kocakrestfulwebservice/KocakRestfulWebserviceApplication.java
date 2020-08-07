0362cab35b1506d496a227542fb197fbdc8f5474
ugurkocak1980 <ugurkocak1980@gmail.com>
2020-08-07 17:29:22 +0200

    Initial commit

---
+++ src/main/java/com/example/kocakrestfulwebservice/KocakRestfulWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kocakrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KocakRestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KocakRestfulWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kocakrestfulwebservice/KocakRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kocakrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KocakRestfulWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
